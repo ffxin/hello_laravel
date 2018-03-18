6a83be441051fb25bca1788d407baea2e938f73e
方新 <935937891@qq.com>
2018-03-18 07:33:14 +0000

    提交文件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by fangxin</div>
+                <div class="title">Hello Laravel! - by fangxin_one</div>
             </div>
         </div>
     </body>
